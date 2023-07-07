60dc1762d4f209784ae559114c532c5e8765848b
Mauricio <maurolores1992@gmail.com>
2023-07-07 01:04:09 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+function guardarDatos() {
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var telefono = document.getElementById('telefono').value;
+    var email = document.getElementById('email').value;
+    var direccion = document.getElementById('direccion').value;
+    
+    var datos = {
+      nombre: nombre,
+      apellido: apellido,
+      telefono: telefono,
+      email: email,
+      direccion: direccion
+    };
+    
+
+    var datosGuardados = JSON.parse(localStorage.getItem('datosSocios')) || [];     // Obtener los datos almacenados previamente (si los hay)
+    datosGuardados.push(datos);   // Agregar los nuevos datos al array
+    localStorage.setItem('datosSocios', JSON.stringify(datosGuardados)); // Guardar los datos actualizados en el localStorage
+    
+    alert('Datos guardados correctamente');
+    
+    // Limpiar el formulario
+    document.getElementById('nombre').value = '';
+    document.getElementById('apellido').value = '';
+    document.getElementById('telefono').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('direccion').value = '';
+}
+
+// Obtener los datos almacenados en el localStorage
+var datosGuardados = JSON.parse(localStorage.getItem('datosSocios')) || [];
+  
+// Recorrer los datos y mostrarlos en la tabla
+for (var i = 0; i < datosGuardados.length; i++) {
+  var datos = datosGuardados[i];
+  var fila = document.createElement('tr');
+
+  var celdaNombre = document.createElement('td');
+  celdaNombre.textContent = datos.nombre;
+  fila.appendChild(celdaNombre);
+
+  var celdaApellido = document.createElement('td');
+  celdaApellido.textContent = datos.apellido;
+  fila.appendChild(celdaApellido);
+
+  var celdaTelefono = document.createElement('td');
+  celdaTelefono.textContent = datos.telefono;
+  fila.appendChild(celdaTelefono);
+
+  var celdaDireccion = document.createElement('td');
+  celdaDireccion.textContent = datos.direccion;
+  fila.appendChild(celdaDireccion);
+
+  var celdaAcceso = document.createElement('td');
+  celdaAcceso.textContent = datos.permisoAcceso ? 'Acceso permitido' : 'Acceso denegado';
+  fila.appendChild(celdaAcceso);
+
+  var celdaCategoria = document.createElement('td');
+  celdaCategoria.textContent = datos.categoria;
+  fila.appendChild(celdaCategoria);
+
+  var celdaMensualidad = document.createElement('td');
+  celdaMensualidad.textContent = datos.mensualidad || '';
+  fila.appendChild(celdaMensualidad);
+
+  document.querySelector('tbody').appendChild(fila);
+}
