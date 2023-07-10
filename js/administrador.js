e0e6e0f6e06d53f6d1a90f6bffff4861c3879a17
Mauricio Lores <111545047+maurolores92@users.noreply.github.com>
2023-07-10 20:18:37 -0300

    Update administrador.js

--- js/administrador.js
+++ js/administrador.js
@@ -2,102 +2,142 @@ var datosGuardados = JSON.parse(localStorage.getItem('datosSocios')) || []; // O
 
 // Función para mostrar el formulario de edición con los datos del afiliado seleccionado
 function mostrarFormularioEdicion(indice) {
-    var datos = datosGuardados[indice];
-    var formulario = document.getElementById('formularioEdicion');
-        formulario.style.display = 'block'; // Mostrar el formulario
-        // Llenar el formulario con los datos del afiliado seleccionado
-        formulario.elements['nombre'].value = datos.nombre;
-        formulario.elements['apellido'].value = datos.apellido;
-        formulario.elements['telefono'].value = datos.telefono;
-        formulario.elements['email'].value = datos.email;
-        formulario.elements['direccion'].value = datos.direccion;
-        formulario.elements['categoria'].value = datos.categoria;
-        formulario.elements['mensualidad'].value = datos.mensualidad || '';
-        formulario.dataset.indice = indice; // Guardar el índice del afiliado en el formulario
+  var datos = datosGuardados[indice];
+  var formulario = document.getElementById('formularioEdicion');
+  formulario.style.display = 'block'; // Mostrar el formulario
+  // Llenar el formulario con los datos del afiliado seleccionado
+  formulario.elements['nombre'].value = datos.nombre;
+  formulario.elements['apellido'].value = datos.apellido;
+  formulario.elements['telefono'].value = datos.telefono;
+  formulario.elements['email'].value = datos.email;
+  formulario.elements['direccion'].value = datos.direccion;
+  formulario.elements['categoria'].value = datos.categoria;
+  formulario.elements['mensualidad'].value = datos.mensualidad || '';
+  formulario.dataset.indice = indice; // Guardar el índice del afiliado en el formulario
+
+  var accesoSwitch = document.getElementById('accesoSwitch');
+  accesoSwitch.checked = datosGuardados[indice].permisoAcceso;
 }
 
 // Función para actualizar los datos del afiliado en la tabla y en el localStorage
 function editarDatos() {
-    var formulario = document.getElementById('formularioEdicion');
-    var indice = parseInt(formulario.dataset.indice);
+  var formulario = document.getElementById('formularioEdicion');
+  var indice = parseInt(formulario.dataset.indice);
 
-    // Obtener los valores del formulario
-    var nombre = formulario.elements['nombre'].value;
-    var apellido = formulario.elements['apellido'].value;
-    var telefono = formulario.elements['telefono'].value;
-    var email = formulario.elements['email'].value;
-    var direccion = formulario.elements['direccion'].value;
-    var categoria = formulario.elements['categoria'].value;
-    var mensualidad = formulario.elements['mensualidad'].value;
+  // Obtener los valores del formulario
+  var nombre = formulario.elements['nombre'].value;
+  var apellido = formulario.elements['apellido'].value;
+  var telefono = formulario.elements['telefono'].value;
+  var email = formulario.elements['email'].value;
+  var direccion = formulario.elements['direccion'].value;
+  var categoria = formulario.elements['categoria'].value;
+  var mensualidad = formulario.elements['mensualidad'].value;
 
-    // Actualizar los datos en el array
-    datosGuardados[indice].nombre = nombre;
-    datosGuardados[indice].apellido = apellido;
-    datosGuardados[indice].telefono = telefono;
-    datosGuardados[indice].email = email;
-    datosGuardados[indice].direccion = direccion;
-    datosGuardados[indice].categoria = categoria;
-    datosGuardados[indice].mensualidad = mensualidad;
+  // Actualizar los datos en el array
+  datosGuardados[indice].nombre = nombre;
+  datosGuardados[indice].apellido = apellido;
+  datosGuardados[indice].telefono = telefono;
+  datosGuardados[indice].email = email;
+  datosGuardados[indice].direccion = direccion;
+  datosGuardados[indice].categoria = categoria;
+  datosGuardados[indice].mensualidad = mensualidad;
 
-    // Actualizar los datos en el localStorage
-    localStorage.setItem('datosSocios', JSON.stringify(datosGuardados));
-    // Actualizar la fila correspondiente en la tabla
-    var fila = document.getElementById('fila-' + indice);
-    fila.cells[0].textContent = nombre;
-    fila.cells[1].textContent = apellido;
-    fila.cells[2].textContent = telefono;
-    fila.cells[3].textContent = email;
-    fila.cells[4].textContent = direccion;
-    fila.cells[6].textContent = categoria;
-    fila.cells[7].textContent = mensualidad || '';
+  // Actualizar los datos en el localStorage
+  localStorage.setItem('datosSocios', JSON.stringify(datosGuardados));
 
-    formulario.style.display = 'none'; // Ocultar el formulario de edición
+  // Actualizar la fila correspondiente en la tabla
+  var fila = document.getElementById('fila-' + indice);
+  fila.cells[0].textContent = nombre;
+  fila.cells[1].textContent = apellido;
+  fila.cells[3].textContent = categoria;
+  fila.cells[4].textContent = mensualidad || '';
+
+  formulario.style.display = 'none'; // Ocultar el formulario de edición
 }
 
 // Recorrer los datos y mostrarlos en la tabla
 for (var i = 0; i < datosGuardados.length; i++) {
+  var datos = datosGuardados[i];
+  var fila = document.createElement('tr');
+  fila.id = 'fila-' + i;
+
+  var celdaNombre = document.createElement('td');
+  celdaNombre.textContent = datos.nombre;
+  fila.appendChild(celdaNombre);
+
+  var celdaApellido = document.createElement('td');
+  celdaApellido.textContent = datos.apellido;
+  fila.appendChild(celdaApellido);
+
+  var celdaPermitirAcceso = document.createElement('td');
+  var interruptor = document.createElement('input');
+  interruptor.type = 'checkbox';
+  interruptor.checked = datos.permisoAcceso;
+  interruptor.addEventListener('change', function(indice) {
+    return function() {
+      cambiarPermisoAcceso(indice, this.checked);
+    };
+  }(i));
+  celdaPermitirAcceso.appendChild(interruptor);
+  fila.appendChild(celdaPermitirAcceso);
+
+  var celdaCategoria = document.createElement('td');
+  celdaCategoria.textContent = datos.categoria;
+  fila.appendChild(celdaCategoria);
+
+  var celdaMensualidad = document.createElement('td');
+  celdaMensualidad.textContent = datos.mensualidad || '';
+  fila.appendChild(celdaMensualidad);
+
+  var celdaAcciones = document.createElement('td');
+  var botonEditar = document.createElement('button');
+  botonEditar.textContent = 'Editar';
+  botonEditar.addEventListener('click', function(indice) {
+    return function() {
+      mostrarFormularioEdicion(indice);
+    };
+  }(i));
+  celdaAcciones.appendChild(botonEditar);
+  fila.appendChild(celdaAcciones);
+
+  document.querySelector('tbody').appendChild(fila);
+}
+// Función para cambiar el permiso de acceso
+function cambiarPermisoAcceso(indice, valor) {
+  datosGuardados[indice].permisoAcceso = valor;
+  localStorage.setItem('datosSocios', JSON.stringify(datosGuardados));
+}
+function descargarDatosPDF() {
+  // Crear un nuevo documento PDF
+  var doc = new jsPDF.default();
+
+  // Obtener los datos almacenados en el localStorage
+  var datosGuardados = JSON.parse(localStorage.getItem('datosSocios')) || [];
+
+  // Configurar las opciones de estilo del texto
+  var options = {
+    align: 'left',
+    fontSize: 12,
+    lineHeight: 15
+  };
+
+  // Generar el contenido del PDF con los datos del array
+  var contenido = '';
+  for (var i = 0; i < datosGuardados.length; i++) {
     var datos = datosGuardados[i];
-    var fila = document.createElement('tr');
-    fila.id = 'fila-' + i;
+    contenido += 'Nombre: ' + datos.nombre + '\n';
+    contenido += 'Apellido: ' + datos.apellido + '\n';
+    contenido += 'Teléfono: ' + datos.telefono + '\n';
+    contenido += 'Dirección: ' + datos.direccion + '\n';
+    contenido += 'Permiso de acceso: ' + datos.permisoAcceso + '\n';
+    contenido += 'Categoría: ' + datos.categoria + '\n';
+    contenido += 'Mensualidad: ' + (datos.mensualidad || '') + '\n';
+    contenido += '\n';
+  }
 
-    var celdaNombre = document.createElement('td');
-    celdaNombre.textContent = datos.nombre;
-    fila.appendChild(celdaNombre);
+  // Agregar el contenido al documento PDF
+  doc.text(contenido, options.align, options.fontSize, options);
 
-    var celdaApellido = document.createElement('td');
-    celdaApellido.textContent = datos.apellido;
-    fila.appendChild(celdaApellido);
-
-    var celdaPermitirAcceso = document.createElement('td');
-    var interruptor = document.createElement('input');
-    interruptor.type = 'checkbox';
-    interruptor.checked = datos.permisoAcceso;
-    interruptor.addEventListener('change', function(indice) {
-    return function() {
-        cambiarPermisoAcceso(indice, this.checked);
-    };
-}(i));
-    celdaPermitirAcceso.appendChild(interruptor);
-    fila.appendChild(celdaPermitirAcceso);
-
-    var celdaCategoria = document.createElement('td');
-    celdaCategoria.textContent = datos.categoria;
-    fila.appendChild(celdaCategoria);
-
-    var celdaMensualidad = document.createElement('td');
-    celdaMensualidad.textContent = datos.mensualidad || '';
-    fila.appendChild(celdaMensualidad);
-
-    var celdaAcciones = document.createElement('td');
-    var botonEditar = document.createElement('button');
-    botonEditar.textContent = 'Editar';
-    botonEditar.addEventListener('click', function(indice) {
-    return function() {
-        mostrarFormularioEdicion(indice);
-    };
-    }(i));
-    celdaAcciones.appendChild(botonEditar);
-    fila.appendChild(celdaAcciones);
-
-    document.querySelector('tbody').appendChild(fila);
-    }
+  // Descargar el archivo PDF
+  doc.save('datos.pdf');
+}
